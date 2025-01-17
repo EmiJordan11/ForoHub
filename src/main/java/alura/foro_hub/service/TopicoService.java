866f8353fe230388d8bd54271870e12289db3733
EmiJordan11 <emiliano.e.jordan@gmail.com>
2025-01-17 13:01:57 -0300

    feat: Crear capa Service para Topico

---
+++ src/main/java/alura/foro_hub/service/TopicoService.java
@@ -0,0 +1,68 @@
+package alura.foro_hub.service;
+
+import alura.foro_hub.dto.topico.ActualizarTopicoDTO;
+import alura.foro_hub.dto.topico.RegistrarTopicoDTO;
+import alura.foro_hub.dto.topico.TopicoDTO;
+import alura.foro_hub.entities.Curso;
+import alura.foro_hub.entities.Topico;
+import alura.foro_hub.entities.Usuario;
+import alura.foro_hub.repository.CursoRepository;
+import alura.foro_hub.repository.TopicoRepository;
+import alura.foro_hub.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class TopicoService {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    //POST
+    public TopicoDTO registrarTopico(RegistrarTopicoDTO datos){
+        Usuario usuario = usuarioRepository.getReferenceById(datos.idAutor());
+        Curso curso = cursoRepository.getReferenceById(datos.idCurso());
+
+        Topico topico = new Topico(datos, usuario, curso);
+        topico = topicoRepository.save(topico);
+
+        //retorno el DTO
+       return new TopicoDTO(topico);
+    }
+
+    //GET
+    public List<TopicoDTO> listarTopicos(){
+        List<Topico> topicos = topicoRepository.findAll();
+        return topicos.stream().map(TopicoDTO::new).toList();
+    }
+
+    //PUT
+    public TopicoDTO actualizarTopico(ActualizarTopicoDTO datos){
+        Topico topico = topicoRepository.getReferenceById(datos.id());
+
+        if (datos.titulo()!=null && !datos.titulo().isBlank()){
+            topico.setTitulo(datos.titulo());
+        }
+        if (datos.mensaje()!=null && !datos.mensaje().isBlank()){
+            topico.setMensaje(datos.mensaje());
+        }
+
+        return new TopicoDTO(topico);
+    }
+
+
+    public void eliminarTopico(Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        //setteo la fecha de baja con fecha actual
+        topico.setFechaHoraBaja(LocalDateTime.now());
+    }
+}
