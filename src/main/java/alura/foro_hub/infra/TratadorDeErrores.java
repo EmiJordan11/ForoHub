61f81a39b56cf115f86700ed8c044448bd10a342
EmiJordan11 <emiliano.e.jordan@gmail.com>
2025-01-17 10:28:06 -0300

    feat: Manejar error 400 y 404

---
+++ src/main/java/alura/foro_hub/infra/TratadorDeErrores.java
@@ -0,0 +1,40 @@
+package alura.foro_hub.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    //ruta inexistente
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity largarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    //argumentos invalidos con @Valid
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity largarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream()
+                .map(ErrorDTO::new).toList();
+
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+
+
+    //DTO
+    public record ErrorDTO(
+            String campo,
+            String error
+    ){
+        public ErrorDTO(FieldError e){
+            this(e.getField(),e.getDefaultMessage());
+        }
+    }
+
+}
